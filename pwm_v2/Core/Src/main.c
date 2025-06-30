1c2c810783c711448515dd81417eced13c3a5732
omwfoster@gmail.com <3420772+omwfoster@users.noreply.github.com>
2025-06-30 16:20:14 +0100

    adding ai_logging and usart to stlink(for cdc coms)

--- pwm_v2/Core/Src/main.c
+++ pwm_v2/Core/Src/main.c
@@ -17,22 +17,21 @@
  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "arm_math.h"
-#include <arm_neon.h>
 #include "main.h"
-#include "stdbool.h"
-
 #include "pdm2pcm.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stdbool.h"
 #include "fft_module.h"
+#include "ai_logging.h"
 
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+
+#define SEND_BUFFER_MAX_SIZE 512
 
 enum {
 	TRANSFER_WAIT, TRANSFER_COMPLETE, TRANSFER_HALF, TRANSFER_ERROR
@@ -80,6 +79,12 @@ uint16_t PDM_Out[16];
 
 __IO uint32_t wTransferState = TRANSFER_WAIT;
 
+ai_logging_device_t device;
+
+
+
+
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -95,6 +100,8 @@ DMA_HandleTypeDef hdma_spi1_rx;
 
 TIM_HandleTypeDef htim1;
 
+USART_HandleTypeDef husart1;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -106,9 +113,12 @@ static void MX_DMA_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_CRC_Init(void);
+static void MX_USART1_Init(void);
 /* USER CODE BEGIN PFP */
 
 void deinterleave(uint16_t *mixed, uint16_t Length);
+uint32_t usart_send_function(uint8_t *, uint32_t);
+void pack_data(ai_logging_packet_t * p);
 
 /* USER CODE END PFP */
 
@@ -118,49 +128,57 @@ void deinterleave(uint16_t *mixed, uint16_t Length);
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
-	/* USER CODE BEGIN 1 */
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
 	uint8_t SPI_In[16];
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
 	/* Filter LP & HP Init */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_DMA_Init();
-	MX_TIM1_Init();
-	MX_SPI1_Init();
-	MX_CRC_Init();
-	MX_PDM2PCM_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_TIM1_Init();
+  MX_SPI1_Init();
+  MX_CRC_Init();
+  MX_PDM2PCM_Init();
+  MX_USART1_Init();
+  /* USER CODE BEGIN 2 */
 
-//	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-	HAL_SPI_Receive_DMA(&hspi1, &t_U_Pdm, INTERNAL_BUFF_SIZE);
 
-	/* USER CODE END 2 */
+  uint8_t send_buffer[SEND_BUFFER_MAX_SIZE];
+  ai_logging_init(&device);
+  ai_logging_init_send(&device, usart_send_function, send_buffer,SEND_BUFFER_MAX_SIZE);
+  HAL_SPI_Receive_DMA(&hspi1, &t_U_Pdm, INTERNAL_BUFF_SIZE);
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  ai_logging_packet_t packet;
+  packet.timestamp = -1;
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1) {
 
 		if (pcm_full != 0x0) {
@@ -169,190 +187,238 @@ int main(void) {
 
 		if (block_ready != 0x0) {
 			fft_test(pcm_deinterleaved);
+			pack_data(&packet);
 			block_ready = false;
 
 	}
 
-	/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-	/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 }
-/* USER CODE END 3 */
-}
-
-/**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
-
-/** Configure the main internal regulator output voltage
- */
-__HAL_RCC_PWR_CLK_ENABLE();
-__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-/** Initializes the RCC Oscillators according to the specified parameters
- * in the RCC_OscInitTypeDef structure.
- */
-RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-RCC_OscInitStruct.PLL.PLLM = 8;
-RCC_OscInitStruct.PLL.PLLN = 96;
-RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-RCC_OscInitStruct.PLL.PLLQ = 4;
-RCC_OscInitStruct.PLL.PLLR = 2;
-if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-	Error_Handler();
-}
-
-/** Initializes the CPU, AHB and APB buses clocks
- */
-RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-		| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK) {
-	Error_Handler();
-}
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief CRC Initialization Function
- * @param None
- * @retval None
- */
-static void MX_CRC_Init(void) {
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-/* USER CODE BEGIN CRC_Init 0 */
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-/* USER CODE END CRC_Init 0 */
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 96;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-/* USER CODE BEGIN CRC_Init 1 */
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-/* USER CODE END CRC_Init 1 */
-hcrc.Instance = CRC;
-if (HAL_CRC_Init(&hcrc) != HAL_OK) {
-	Error_Handler();
-}
-__HAL_CRC_DR_RESET(&hcrc);
-/* USER CODE BEGIN CRC_Init 2 */
-
-/* USER CODE END CRC_Init 2 */
-
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
- * @brief SPI1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_SPI1_Init(void) {
+  * @brief CRC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_CRC_Init(void)
+{
 
-/* USER CODE BEGIN SPI1_Init 0 */
+  /* USER CODE BEGIN CRC_Init 0 */
 
-/* USER CODE END SPI1_Init 0 */
+  /* USER CODE END CRC_Init 0 */
 
-/* USER CODE BEGIN SPI1_Init 1 */
+  /* USER CODE BEGIN CRC_Init 1 */
 
-/* USER CODE END SPI1_Init 1 */
-/* SPI1 parameter configuration*/
-hspi1.Instance = SPI1;
-hspi1.Init.Mode = SPI_MODE_MASTER;
-hspi1.Init.Direction = SPI_DIRECTION_2LINES_RXONLY;
-hspi1.Init.DataSize = SPI_DATASIZE_16BIT;
-hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-hspi1.Init.CLKPhase = SPI_PHASE_2EDGE;
-hspi1.Init.NSS = SPI_NSS_SOFT;
-hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
-hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-hspi1.Init.CRCPolynomial = 15;
-if (HAL_SPI_Init(&hspi1) != HAL_OK) {
-	Error_Handler();
-}
-/* USER CODE BEGIN SPI1_Init 2 */
+  /* USER CODE END CRC_Init 1 */
+  hcrc.Instance = CRC;
+  if (HAL_CRC_Init(&hcrc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  __HAL_CRC_DR_RESET(&hcrc);
+  /* USER CODE BEGIN CRC_Init 2 */
 
-/* USER CODE END SPI1_Init 2 */
+  /* USER CODE END CRC_Init 2 */
 
 }
 
 /**
- * @brief TIM1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_TIM1_Init(void) {
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
 
-/* USER CODE BEGIN TIM1_Init 0 */
+  /* USER CODE BEGIN SPI1_Init 0 */
 
-/* USER CODE END TIM1_Init 0 */
+  /* USER CODE END SPI1_Init 0 */
 
-TIM_ClockConfigTypeDef sClockSourceConfig = { 0 };
-TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+  /* USER CODE BEGIN SPI1_Init 1 */
 
-/* USER CODE BEGIN TIM1_Init 1 */
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES_RXONLY;
+  hspi1.Init.DataSize = SPI_DATASIZE_16BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_2EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 15;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
 
-/* USER CODE END TIM1_Init 1 */
-htim1.Instance = TIM1;
-htim1.Init.Prescaler = 1;
-htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-htim1.Init.Period = 47;
-htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-htim1.Init.RepetitionCounter = 0;
-htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-if (HAL_TIM_Base_Init(&htim1) != HAL_OK) {
-	Error_Handler();
-}
-sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK) {
-	Error_Handler();
-}
-sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK) {
-	Error_Handler();
-}
-/* USER CODE BEGIN TIM1_Init 2 */
-
-/* USER CODE END TIM1_Init 2 */
+  /* USER CODE END SPI1_Init 2 */
 
 }
 
 /**
- * Enable DMA controller clock
- */
-static void MX_DMA_Init(void) {
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
 
-/* DMA controller clock enable */
-__HAL_RCC_DMA2_CLK_ENABLE();
+  /* USER CODE BEGIN TIM1_Init 0 */
 
-/* DMA interrupt init */
-/* DMA2_Stream0_IRQn interrupt configuration */
-HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
-HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 1;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 47;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
 
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void) {
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  husart1.Instance = USART1;
+  husart1.Init.BaudRate = 115200;
+  husart1.Init.WordLength = USART_WORDLENGTH_8B;
+  husart1.Init.StopBits = USART_STOPBITS_1;
+  husart1.Init.Parity = USART_PARITY_NONE;
+  husart1.Init.Mode = USART_MODE_TX_RX;
+  husart1.Init.CLKPolarity = USART_POLARITY_LOW;
+  husart1.Init.CLKPhase = USART_PHASE_1EDGE;
+  husart1.Init.CLKLastBit = USART_LASTBIT_DISABLE;
+  if (HAL_USART_Init(&husart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA2_Stream0_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
-/* GPIO Ports Clock Enable */
-__HAL_RCC_GPIOA_CLK_ENABLE();
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
@@ -411,19 +477,72 @@ if (next_cursor <= end_output_block) {
 wTransferState = TRANSFER_HALF;
 }
 
+uint32_t usart_send_function(uint8_t *data_ptr, uint32_t data_size)
+{
+ /* int32_t bytesLeft = data_size;
+  while(bytesLeft > 0)
+  {
+    while(((USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData)->TxState != 0);
+    if(bytesLeft - MAX_SEND_PACKET_SIZE <= 0)
+    {
+      USBD_CDC_SetTxBuffer(&hUsbDeviceFS, (uint8_t*)data_ptr+(data_size - bytesLeft), bytesLeft);
+      bytesLeft = 0;
+    }
+    else
+    {
+      USBD_CDC_SetTxBuffer(&hUsbDeviceFS, (uint8_t*)data_ptr+(data_size - bytesLeft), MAX_SEND_PACKET_SIZE);
+      bytesLeft = bytesLeft - MAX_SEND_PACKET_SIZE;
+    }
+
+    if(USBD_CDC_TransmitPacket(&hUsbDeviceFS) != USBD_OK)
+    {
+      while(1);
+    }
+
+    while(((USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData)->TxState != 0);
+    USBD_CDC_SetTxBuffer(&hUsbDeviceFS, (uint8_t*)data_ptr, 0);
+    if(USBD_CDC_TransmitPacket(&hUsbDeviceFS) != USBD_OK)
+    {
+      while(1);
+    }
+    HAL_Delay(1);
+  }
+  return data_size; // Ok all data have been sent*/
+}
+
+void pack_data(ai_logging_packet_t * p)
+{
+
+      p->message = "Accelero";
+      p->message_size = strlen(p->message);
+   //   p->payload = (uint8_t*)&acc_axes;
+    //  p->payload_size = sizeof(acc_axes);
+   //   ai_logging_create_shape_1d(* shape, 3);
+      p->payload_type = AI_INT32;
+      p->timestamp = HAL_GetTick();
+      ai_logging_send_packet(&device, p);
+
+}
+
+
+
+
+
+
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-/* USER CODE BEGIN Error_Handler_Debug */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
 /* User can add his own implementation to report the HAL error return state */
 __disable_irq();
 while (1) {
 }
-/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
